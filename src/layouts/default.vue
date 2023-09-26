8bdf1b02d4015751b5c222cd301019dc86c58e99
Jesse <gogoxoom@gmail.com>
2023-09-26 10:12:00 -0500

    updated new sections

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    <MetaTags />
-    <div
-      class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50"
-    >
-      <div class="shadow">
-        <slot />
-      </div>
+    <div>
+        <MetaTags/>
+        <div class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50">
+            <div class="shadow">
+                <slot/>
+            </div>
+            <TheFooter client:none></TheFooter>
+        </div>
     </div>
-  </div>
 </template>
 <style></style>
